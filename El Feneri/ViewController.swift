7ee4a63d1010d021b4bd81c5a911f7fe270de251
Ayça Işık <117450558+aycaisik@users.noreply.github.com>
2022-11-26 18:01:20 +0300

    Added function

--- El Feneri/ViewController.swift
+++ El Feneri/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: -UI Elements
+    @IBOutlet var liightButton: UIButton!
 
+    //MARK: -Properties
+    var lightOn = true
+    
+    //MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI() //uygulama ekrana gelmeden başlangıçta buttonun ismini değiştirebildik
     }
-
+    
+    //MARK: -Actions
+    @IBAction func buttonTapped(_ button:UIButton){
+        lightOn.toggle() // true ise false false ise true yapar
+        //lightOn = !lightOn da yapabilirdik.
+        updateUI()
+    }
+    
+    //MARK: -Functions
+    func updateUI(){
+        view.backgroundColor = lightOn ? .white : .black
+        
+        if lightOn {
+        //ışık açık olmalı
+        view.backgroundColor = .white
+        //liightButton.setTitle("OFF", for: .normal)
+    
+        }else{
+        //ışık kapalı olmalı
+        view.backgroundColor = .black
+        //liightButton.setTitle("ON", for: .normal)
+            
+        }
+    }
 
 }
 
